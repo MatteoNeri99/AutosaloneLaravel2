97b988ba8dd76dde02c90060fa5731b1dd378673
MatteoNeri99 <matteolavoro99@gmail.com>
2025-05-13 17:16:07 +0200

    aggiunto testo per la forceDelete
    ho cambiato l'ordine delle funzioni in modo tale che prima l'auto venga creata poi venga modificata poi vengano controllate le rotte che hanno come id l'id del auto creata e poi in fine venga eliminata definitivamente cosi che il giro possa riiniziare

--- tests/Feature/HTTPTest.php
+++ tests/Feature/HTTPTest.php
@@ -8,40 +8,8 @@ use Tests\TestCase;
 
 class HTTPTest extends TestCase
 {
-    //controllo caricamento page
-    public function test_caricamento_page(){
 
-        //creo un profili fake per permettere l'accesso alle page
-        $user = \App\Models\Admin::factory()->create();
-        $this->actingAs($user);
-
-        //controllo caricamento homepage
-       $this->get('/home')->assertStatus(200);
-
-        //controllo caricamento auto (index)
-       $this->get('/auto')->assertStatus(200);
-
-        //controllo caricamento page messaggi ricevuti
-       $this->get('/admin/messages')->assertStatus(200);
-
-        //controllo caricamento page per aggiungere auto (create)
-       $this->get('/auto/create')->assertStatus(200);
-
-        //controllo caricamento page per modificare auto (edit)
-       $this->get('/auto/1/edit')->assertStatus(200);
-
-       //controllo caricamento page che mostra una sola auto (show)
-       $this->get('/auto/1')->assertStatus(200);
-
-        //controllo caricamento page che mostra le auto sof deleted (trashed)
-       $this->get('/cestino')->assertStatus(200);
-
-
-    }
-
-
-
-    // funzione per controllare function del AutoController
+        // funzione per controllare function del AutoController
     public function test_function_AutoController(){
 
         //disabilitato CSRF
@@ -105,8 +73,10 @@ class HTTPTest extends TestCase
 
         //controlla che la funzione di ricerca funzioni (search)
        $response = $this->get('auto/search?q=nuova')->assertStatus(200);
+
        //controllo che nel HTML sia presente la parola 'nuova'
        $response->assertSee('nuova');
+
        //controllo che nel HTML non sia presente la parola 'usata'
        $response->assertDontSee('usata');
 
@@ -114,11 +84,54 @@ class HTTPTest extends TestCase
 
        //controllo la funzione di soft delete
        $this->delete('/auto/1')->assertStatus(302);
+
        //controllo che sia stato effettivamente soft deleted
        $this->assertSoftDeleted('autos', ['id' => 1]);
+
        //controllo funzionamento di ripristino auto (restore)
        $this->get('/cestino/ripristina/1')->assertStatus(302);
 
 
     }
+
+
+    //controllo caricamento page
+    public function test_caricamento_page(){
+
+        //creo un profili fake per permettere l'accesso alle page
+        $user = \App\Models\Admin::factory()->create();
+        $this->actingAs($user);
+
+        //controllo caricamento homepage
+       $this->get('/home')->assertStatus(200);
+
+        //controllo caricamento auto (index)
+       $this->get('/auto')->assertStatus(200);
+
+        //controllo caricamento page messaggi ricevuti
+       $this->get('/admin/messages')->assertStatus(200);
+
+        //controllo caricamento page per aggiungere auto (create)
+       $this->get('/auto/create')->assertStatus(200);
+
+        //controllo caricamento page per modificare auto (edit)
+       $this->get('/auto/1/edit')->assertStatus(200);
+
+       //controllo caricamento page che mostra una sola auto (show)
+       $this->get('/auto/1')->assertStatus(200);
+
+        //controllo caricamento page che mostra le auto sof deleted (trashed)
+       $this->get('/cestino')->assertStatus(200);
+
+
+    }
+
+    public function test_forceDelete(){
+
+        $this->get('/cestino/elimina/1')->assertStatus(302);
+    }
+
+
+
+
 }
