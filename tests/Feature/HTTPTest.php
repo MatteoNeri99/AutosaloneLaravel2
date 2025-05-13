ec495a0a1141a850a1a68aeb538c16c11af73b10
MatteoNeri99 <matteolavoro99@gmail.com>
2025-05-13 10:53:46 +0200

    aggiunti controll caricamento di tutte le page

--- tests/Feature/HTTPTest.php
+++ tests/Feature/HTTPTest.php
@@ -12,5 +12,25 @@ class HTTPTest extends TestCase
 
         //controllo caricamento homepage
        $this->get('/home')->assertStatus(200);
+
+        //controllo caricamento auto (index)
+       $this->get('/auto')->assertStatus(200);
+
+        //controllo caricamento page messaggi ricevuti
+       $this->get('/admin/messages')->assertStatus(200);
+
+        //controllo caricamento page per aggiungere auto
+       $this->get('/auto/create')->assertStatus(200);
+
+        //controllo caricamento page per modificare auto
+       $this->get('/auto/1/edit')->assertStatus(200);
+
+       //controllo caricamento page che mostra una sola auto (show)
+       $this->get('/auto/1')->assertStatus(200);
+
+        //controllo caricamento page che mostra le auto sof deleted
+       $this->get('/cestino')->assertStatus(200);
+
+
     }
 }
